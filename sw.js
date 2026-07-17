eb8ea6de120b0f6195ab7628137cad0564ef54b2
9matsuochiho-hub <9matsuochiho@gmail.com>
2026-07-17 17:13:01 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+/* コマドリ Service Worker
+   アプリ本体をキャッシュして、電波がなくても開けるようにします。
+   ファイルを更新したら、下の CACHE の数字を1つ増やしてください。 */
+const CACHE = 'komadori-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-180.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-512-maskable.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
